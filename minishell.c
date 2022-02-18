e7aa30d33ce7ca6fa459af96a4d40b1e3ca80945
oreol4 <soldatovandrey4@gmail.com>
2022-02-18 19:05:27 +0300

    rwqg

--- minishell.c
+++ minishell.c
@@ -1,23 +1,11 @@
 #include <sys/stat.h> //fixme delete
 #include "minishell.h"
 
-
 void	ft_free_list(t_comm *lst)
 {
 	t_comm *head;
 	t_envp *head2;
 
-	// while (lst->e)
-	// {
-	// 	// printf("")
-	// 	head2 = lst->e;
-	// 	if (lst->e->key)
-	// 		free (lst->e->key);
-	// 	if (lst->e->value)
-	// 		free (lst->e->value);
-	// 	lst->e = lst->e->next;
-	// 	free (head2);
-	// }
 	while (lst)
     {
         head = lst;
@@ -33,119 +21,22 @@ void	ft_free_list(t_comm *lst)
     }
 }
 
-char *ft_get_key(int count, char **env)
-{
-	char *str;
-	int i;
 
-	i = 0;
-	while (env[i])
-	{
-		if (i == count)
-		{
-			int z = 0;
-			while (env[i][z] && env[i][z] != '=')
-				z++;
-			str = ft_substr(env[i], 0, z);
-			break ;
-		}
-		i++;
-	}
-	return (str);
-}
-
-char *ft_get_value(int count, char **env)
-{
-	char *str;
-	int i;
-
-	i = 0;
-	while (env[i])
-	{
-		if (i == count)
-		{
-			int z = 0;
-			while (env[i][z] && env[i][z] != '=')
-				z++;
-			str = ft_substr(env[i], z + 1, ft_strlen(env[i]) - z);
-			break ;
-		}
-		i++;
-	}
-	return (str);
-}
-
-t_envp *ft_node_env(t_envp *e, char **env)
-{
-	int i;
-	int count;
-	t_envp *tmp;
-
-	count = 0;
-	i = 0;
-	while (env[count])
-		count++;
-	e = NULL;
-	while (count-- > 0)
-	{
-		tmp = malloc(sizeof(t_envp));
-		if (!tmp)
-			return (NULL);
-		tmp->key = ft_get_key(count, env);
-		tmp->value = ft_get_value(count, env);
-		tmp->key_orig = ft_get_key(count, env);
-		tmp->value_orig = ft_get_value(count, env);
-		tmp->count = count;
-		tmp->next = e;
-		e = tmp;
-	}
-	return (e);
-}
-
-char	**ft_update_env(t_envp *l_envp)
-{
-	char **new_env;
-	int i;
-	t_envp *tmp;
-
-	tmp = l_envp;
-	i = 0;
-	new_env = (char **)malloc(sizeof(char *) * tmp->count + 1);
-	if (!new_env)
-		return (NULL);
-	while (tmp)
-	{
-		if (tmp->key && tmp->value)
-		{
-			new_env[i] = ft_strdup(tmp->key);
-			new_env[i] = ft_strjoin(new_env[i], "=");
-			new_env[i] = ft_strjoin(new_env[i], tmp->value);
-		}
-//		if (tmp->value)
-//			new_env[i] = ft_strjoin(new_env[i], tmp->value);
-		i++;
-//		printf("|%s| |%s|\n", tmp->key, tmp->value);
-		tmp = tmp->next;
-	}
-	new_env[i] = NULL;
-	return (new_env);
-}
-
-int ft_process4(char *str, t_envp *envp)
+int ft_process4(char *str, t_envp *list_env)
 {
     t_comm *lst;
-	char **env;
+	char **new_env;
 
-	env = ft_update_env(envp);
     lst = malloc(sizeof(t_comm));
     if (!lst)
         return (-1);
     ft_memset((void *)lst, 0, sizeof(t_comm));
-   	lst = ft_parser4(lst, str, envp);
-	if (executor(lst, env) == -1)
-		return (1);
-	// executor(lst, env);
-	//env = ft_update_env(envp); /////
+	new_env = ft_update_env(list_env);
+   	lst = ft_parser4(lst, str, list_env);
+	// if (executor(lst, new_env) == -1)
+	//  	return (1);
+	// printf("ABC\n");
+	executor(lst, new_env);
 	// ft_free_list(lst);
     return (0);
 }
@@ -163,71 +54,70 @@ void	ft_no_malloc(char **str)
 	free (str);
 }
 
-char **ft_get_envp(char **env)
+void	handler(int sig)
 {
-	char **envp;
-	int i = 0;
+	(void)sig;
+	rl_on_new_line();
+	rl_redisplay();
+	write(1,"  \b\b\n", 5);
+	rl_on_new_line();
+	rl_replace_line("", 1);
+	rl_redisplay();
+	// g_error_status = 
+	// g_error = 1;
+}
 
-	while (env[i])
-		i++;
-	envp = (char **)malloc(sizeof(char *) * i + 1);
-	if (!envp)
-		return (NULL);
-	i = 0;
-	while (env[i])
-	{
-		envp[i] = ft_substr(env[i], 0, ft_strlen(env[i]));
-		i++;
+
+void	ft_up_shlvl(t_envp *list_env)
+{
+	t_envp *head;
+	int shlvl;
+	char *tmp;
+	int locate;
+
+	locate = 0;
+	head = list_env;
+	
+	locate = locate_env_key(head, "SHLVL", 0);
+	if (locate == -1)
+		add_to_env(list_env, "SHLVL", "1", 0);
+	else
+	{	
+		shlvl = ft_atoi(get_env_value(list_env, locate, 0)) + 1;
+		tmp = ft_itoa(shlvl);
+		upd_env_value(list_env, tmp, locate, 0);
 	}
-	envp[i] = NULL;
-	return (envp);
+	// printf("%s\n", get_env_value(list_env, locate, 0));
 }
-
-int ft_check_str(char *str)
-{
-    int i;
-
-    i = 0;
-    while (str[i])
-    {
-        if (str[i] == '>' && !(ft_isdigit(str[i + 1])))
-        {
-            i += 2;
-            if (str[i] == '>')
-                return (-1);
-        }
-        i++;
-    }
-    return (1);
-}
-
-int main(void)
+int main(int ac, char **av, char **env)
 {
 	char		*str;
 	char		*name;
-	//char		**env;
-	t_envp		*l_envp;
-	extern char	**environ;
-	
-	l_envp = malloc(sizeof(t_envp));
-	if (!l_envp)
+	char		**envp;
+	t_envp		*list_env;
+
+	list_env = malloc(sizeof(t_envp));
+	if (!list_env)
 		return (-1);
-//	env = ft_get_envp(environ);
-	l_envp = ft_node_env(l_envp, environ);
+	envp = ft_get_envp(env);
+	list_env = ft_node_env(list_env, envp);
+	// ft_up_shlvl(list_env);
 	while (1)
     {
+		signal(SIGINT, handler);
+		signal(SIGQUIT, SIG_IGN);
         str = readline("bash:");
-        if (str && *str)
+        if (str && *str) 
         {
-            add_history(str);
-            // if (ft_lexer(str) != -1)
-            // {
-            if (ft_process4(str, l_envp) == -1)
-                exit(0);
-            // }
-            // else
-                // free (str);
+           	add_history(str);
+            if (ft_lexer(str) != -1)
+            {
+				if (ft_process4(str, list_env) == -1)
+					exit(0);
+            }
+            else
+                free (str);
         }
 	}
-	//ft_n//o_malloc(l_envp); //fixme
+	return (0);
 }

8df2deccfc24d43cfceccb34c717912c2098abef
Andrey <forstudyandrey4@gmail.com>
2022-02-27 00:18:38 +0300

    new exec

--- srcs/executor/pipex/pipex.c
+++ srcs/executor/pipex/pipex.c
@@ -6,11 +6,10 @@
 /*   By: galetha <galetha@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 15:08:07 by wjonatho          #+#    #+#             */
-/*   Updated: 2022/02/22 14:08:20 by galetha          ###   ########.fr       */
+/*   Updated: 2022/02/22 12:43:37 by galetha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <crt_externs.h>
 #include "pipex.h"
 #include <termios.h>
 
@@ -32,7 +31,6 @@ void	handler22(int sig)
 		g_error_status = 131;
 	}
 }
-
 
 void	ft_handler_herdoc(int sig)
 {
@@ -62,10 +60,11 @@ void wait_childs(int n)
 		wait(&status);
 		if (WEXITSTATUS(status) && status != 0)
 		{
-			g_error_status = 127;
-			// printf("%sexit status = %d%s\n",RED, WIFEXITED(status), RESET);
-			fflush(NULL);
+			printf("THIS\n");
+			g_error_status = WEXITSTATUS(status);
 		}
+		else if (status == 0)
+			g_error_status = 0;
 		i++;
 	}
 }
@@ -97,7 +96,7 @@ void	handler_her(int sig)
 	// (void)sig;
 	if (sig == SIGINT)
 	{
-	// printf("A\n");
+		// printf("A\n");
 		g_error_status = 1;
 		exit (g_error_status);
 	}
@@ -106,6 +105,7 @@ void	handler_her(int sig)
 
 	// }
 }
+
 void	ff(int sig)
 {
 	(void) sig;
@@ -122,11 +122,6 @@ void	ff(int sig)
 	exit (g_error_status);
 }
 
-
-
-
-
-
 static inline void heredoc(t_comm *tmp)
 {
 	char	*str;
@@ -137,15 +132,13 @@ static inline void heredoc(t_comm *tmp)
 	{
 		str = readline("> ");
 		if (str == NULL)
-			exit (0);
-		if (ft_strncmp(str, tmp->here, here_len + 1) == 0) ///"tmp.here\0", so+1
-			exit (0);
+			exit (EXIT_SUCCESS);
+		else if (ft_strncmp(str, tmp->here, here_len + 1) == 0)
+			exit (EXIT_SUCCESS);
 		ft_putendl_fd(str, tmp->infile);
 		free(str);
 	}
-	free(str);
 }
-
 
 static inline void redirect(t_comm *tmp)
 {
@@ -153,7 +146,15 @@ static inline void redirect(t_comm *tmp)
 
 	err = 0;
 	if (tmp->infile != FD_UNUSED)
+	{
+		// if (tmp->here != NULL) /// test cmd < 21 << pop | cmd << pop < 21
+		// {
+		// 	heredoc(tmp);
+		// 	close(tmp->infile);
+		// 	tmp->infile = open(".tmp", O_RDONLY);
+		// }
 		err = dup2(tmp->infile, STDIN_FILENO);
+	}
 	if (err != 0)
 		perror("1redirect:");
 	err = 0;
@@ -168,15 +169,18 @@ static inline int *open_pipes(t_comm *tmp)
 	int i;
 	int	*pipes;
 
-	pipes = ft_calloc(sizeof(int), 2 * (tmp->count_node - 1));
-	if (!(pipes))
-		perror("open_pipes: can't malloc");
-	i = 0;
-	while (i < (tmp->count_node - 1))
+	if (tmp->count_node > 1)
 	{
-		if (pipe(pipes + 2 * i) == -1)
-			perror("open_pipes: can't open pipe");
-		i++;
+		pipes = ft_calloc(sizeof(int), 2 * (tmp->count_node - 1));
+		if (!(pipes))
+			perror("open_pipes: can't malloc");
+		i = 0;
+		while (i < (tmp->count_node - 1))
+		{
+			if (pipe(pipes + 2 * i) == -1)
+				perror("open_pipes: can't open pipe");
+			i++;
+		}
 	}
 	return (pipes);
 }
@@ -203,23 +207,29 @@ static inline void pipe_switch(int i, int kind, int *pipes, t_comm *tmp)
 		if (kind == START && tmp->next != NULL)
 		{
 			///    1 =>
-			dup2(pipes[2 * i + 1], STDOUT_FILENO); ///1
+			dup2(pipes[1], STDOUT_FILENO); ///1
 		}
 		else if (kind == MIDDLE)
 		{
 			/// => 2 =>
-			dup2(pipes[2 * i - 2], STDIN_FILENO); ///0
-			dup2(pipes[2 * i + 1], STDOUT_FILENO); ///3
+			dup2(pipes[0], STDIN_FILENO); ///0
+			dup2(pipes[3], STDOUT_FILENO); ///3
 		}
 		else if (kind == END)
 		{
 			/// => 3
-			dup2(pipes[2 * i - 2], STDIN_FILENO); ///2
+			dup2(pipes[2], STDIN_FILENO); ///2
 		}
 	}
 }
 
-
+void	bash_error(char *first_part, char *cmd, char *last_part)
+{
+	ft_putstr_fd(first_part, 2);
+	ft_putstr_fd(cmd, 2);
+	ft_putendl_fd(last_part, 2);
+	g_error_status = 127;
+}
 
 void	handler2(int sig)
 {
@@ -228,17 +238,21 @@ void	handler2(int sig)
 		(void) sig;
 		write(2, "\n", 1);
 		g_error_status = 130;
+		// exit (0);
 	}
 	if (sig == SIGQUIT)
 	{
 		(void) sig;
 		write(2, "Quit: 3\n", 8);
 		g_error_status = 131;
+		// exit (0);
 	}
 }
+
 int pipex(t_comm *lst, char **env)
 {
-
+	//for test:  ls -l | head -6 | cut -b 1-10
+	// echo p | echo r | echo i | echo v | echo e | echo t
 	int		i;
 	int		*pipes;
 	t_comm	*tmp;
@@ -251,7 +265,7 @@ int pipex(t_comm *lst, char **env)
 	pipes = open_pipes(tmp);
 	kind = START;
 	i = 0;
-	while (tmp != NULL)
+	while (tmp != NULL && tmp->cmd)
 	{
 		if (tmp->count_node == 1)
 		{
@@ -262,14 +276,15 @@ int pipex(t_comm *lst, char **env)
 			{
 				pid = fork();
 				if (pid != 0)
+				{
 					signal(SIGINT, SIG_IGN);
-				if (pid == 0) /// test cmd < 21 << pop | cmd << pop < 21
+					signal(SIGQUIT, SIG_IGN);
+				}
+				if (pid == 0)
 				{
 					signal(SIGINT, ff);
+					signal(SIGQUIT, SIG_IGN);
 					heredoc(tmp);
-					close(tmp->infile);
-					unlink(".tmp");
-					exit(0);
 				}
 				wait(&status);
 				if (WIFEXITED(status) && WEXITSTATUS(status) == 1)
@@ -290,13 +305,11 @@ int pipex(t_comm *lst, char **env)
 			bool = builtins(tmp, env);
 			if (bool != -1)
 				exit (bool);
-			if (execve(find_command_path(tmp->command_str[0], env),
-					   tmp->command_str, env) == -1)
+			if (execve(find_command_path(tmp->cmd[0], env),
+					   tmp->cmd, env) == -1)
 			{
-				ft_putstr_fd("bash: ", 2);
-				ft_putstr_fd(tmp->command_str[0], 2);
-				ft_putendl_fd(": command not found", 2);
-				exit(EXIT_FAILURE);
+				bash_error("bash: ", tmp->cmd[0], ": command not found");
+				exit(g_error_status);
 			}
 		}
 		if (tmp->here)
@@ -308,6 +321,8 @@ int pipex(t_comm *lst, char **env)
 	tmp = lst;
 	close_pipes(pipes, tmp->count_node);
 	close_in_out_file(tmp); /// ??? it doesn't close in each node | mb no need
-	wait_childs( tmp->count_node);
+	wait_childs(tmp->count_node);
+	if (lst->count_node > 1)
+		free(pipes);
 	return (EXIT_SUCCESS);
 }

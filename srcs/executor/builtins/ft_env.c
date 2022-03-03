c298ca06107593cabbacb6559fabb815a239c180
oreol4 <soldatovandrey4@gmail.com>
2022-03-03 14:01:58 +0300

    replace printf on putstr

--- srcs/executor/builtins/ft_env.c
+++ srcs/executor/builtins/ft_env.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_env.c                                            :+:      :+:    :+:  */
+/*   ft_env.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wjonatho <wjonatho@student.21-school.ru>   +#+  +:+       +#+        */
+/*   By: galetha <galetha@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/20 15:08:07 by wjonatho          #+#    #+#             */
-/*   Updated: 2022/02/20 17:53:20 by wjonatho         ###   ########.fr       */
+/*   Updated: 2022/03/03 14:01:23 by galetha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,9 @@ int	ft_env(t_comm *lst)
 	{
 		if (tmp->key && tmp->value)
 		{
-			printf("%s=%s\n", tmp->key, tmp->value);
-			/*ft_putstr_fd(tmp->key, STDOUT_FILENO);
+			ft_putstr_fd(tmp->key, STDOUT_FILENO);
 			ft_putchar_fd('=', STDOUT_FILENO);
-			ft_putendl_fd(tmp->value, STDOUT_FILENO);*/
+			ft_putendl_fd(tmp->value, STDOUT_FILENO);
 		}
 		tmp = tmp->next;
 	}
